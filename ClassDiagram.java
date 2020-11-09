32879f211afc6f341ba70c606d61606a8bc433b7
yusufyp <51522321+yusufyp@users.noreply.github.com>
2020-11-09 21:58:22 +0700

    Add Class Diagram

---
+++ ClassDiagram.java
@@ -0,0 +1,119 @@
+package prakppl;
+
+class User {
+
+    String username;
+    String password;
+    String onlineStatus;
+    String name;
+
+    void updateProfile() {
+
+    }
+
+    void login() {
+        onlineStatus = "Online";
+    }
+
+    void logout() {
+        onlineStatus = "Offline";
+    }
+}
+
+class Admin extends User {
+
+    int kontribusi;
+
+    void tambahSertifikasi() {
+
+    }
+
+    void tambahMateri() {
+
+    }
+
+    void hapusMateri() {
+
+    }
+
+    void buatLearningPath() {
+
+    }
+
+    void memverifikasi() {
+
+    }
+}
+
+class Pelajar extends User {
+
+    String pilihanProfesi;
+    int learningPoint;
+    progress[] object;
+    boolean verificationStatus;
+
+    void buatThread() {
+
+    }
+
+    void berkomentar() {
+
+    }
+
+    void masukForum() {
+
+    }
+
+    void aksesMateri() {
+
+    }
+
+    void aksesSertifikasi() {
+
+    }
+
+    void editThread() {
+
+    }
+}
+
+class Forum {
+
+    boolean status;
+    Thread[] object;
+    Thread[] fetchThread() {
+        return object;
+    }
+}
+
+class Thread {
+
+    String id;
+    int member;
+    replies[] object;
+    replies[] fetchReplies() {
+        return object;
+    }
+
+    int fetchMember() {
+        return member;
+    }
+}
+
+class Materi {
+
+    String id;
+    String judul;
+    String sumber;
+    String author;
+    learningPath[] object;
+    //learning path array object
+}
+
+public class PrakPPL {
+
+    public static void main(String[] args) {
+
+    }
+
+}
